5173a27ffdaf733cba33b23b21869182294c22e9
Jonny Burch <jonny@progressionapp.com>
2020-04-01 10:09:54 +0100

    donorbox data imported

--- bin/pull-donorbox.py
+++ bin/pull-donorbox.py
@@ -8,6 +8,7 @@ import cmd
 
 DONORBOX_EMAIL = os.environ.get('DONORBOX_EMAIL', None)
 DONORBOX_API_KEY = os.environ.get('DONORBOX_API_KEY', None)
+CAMPAIGN_ID = os.environ.get('CAMPAIGN_ID', None)
 
 if not DONORBOX_API_KEY or not DONORBOX_EMAIL:
     print("Donorbox environment variables not configured, exiting.")
@@ -17,14 +18,18 @@ if not DONORBOX_API_KEY or not DONORBOX_EMAIL:
 url = "https://{}:{}@donorbox.org/api/v1/campaigns".format(DONORBOX_EMAIL, DONORBOX_API_KEY)
 headers = {'content-type': 'application/json'}
 response = requests.get(url, headers=headers)
-print(response.text)
+print(json.loads(response.text))
+raw_data = json.loads(response.text)
+
+# Get the right campaign
+campaign = next(campaign for campaign in raw_data if campaign["id"] == 155698)
+print(campaign)
 
 FILENAME = 'donorbox.json'
 OUTPUT_DIR = 'data'
 
 path = OUTPUT_DIR + '/' + FILENAME
 
-print(path)
 
 # Trying to mimic the time format from the bash implementation
 now = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
@@ -41,14 +46,13 @@ def save(data_to_save, path):
         json.dump(data_to_save, fp, ensure_ascii=False)
 
 
-# def main():
-#     for table, destination in TABLE_TO_FILENAME.items():
-#         records = get_records(table)
-#         to_save = {"updated": now, "records": records}
-#         target_path = os.path.join(OUTPUT_DIR, destination)
-#         save(to_save, target_path)
-#         print("Creating " + table + " table")
+def main():
+    records = campaign
+    to_save = {"updated": now, "total": records["total_raised"], "data": records}
+    target_path = os.path.join(OUTPUT_DIR, FILENAME)
+    save(to_save, target_path)
+    print("Creating donorbox datafile")
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
