164da22343b4f4e166ad3d002ad8294ff9e04c28
Will Gallia <will@stillatplay.com>
2020-04-02 11:02:45 +0100

    use builtime fallback for donations and poll new donations

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,19 +5,30 @@ import '@modules/lazyload'
 
 const thousands = s => s.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").split('.')[0]
 
-fetch('/api/pull-donorbox')
-  .then(r => r.json())
-  .then((json) => {
-    const { donorboxRaised, donorboxCount } = json
+function grabDonorboxAmounts() {
+  try {
+    fetch('/api/pull-donorbox')
+      .then(r => r.json())
+      .then((json) => {
+        const { donorboxRaised, donorboxCount } = json
 
-    document.querySelectorAll('div.progress-bar').forEach((div) => {
-      const raised = div.querySelector('p.raised')
-      const totalAmount = parseFloat(raised.dataset.amount) + donorboxRaised
-      const donors = div.querySelector('p.donors')
-      const totalDonors = parseFloat(donors.dataset.amount) + donorboxCount
+        document.querySelectorAll('div.progress-bar').forEach((div) => {
+          const raised = div.querySelector('p.raised')
+          const totalAmount = parseFloat(raised.dataset.amount) + donorboxRaised
+          const donors = div.querySelector('p.donors')
+          const totalDonors = parseFloat(donors.dataset.amount) + donorboxCount
 
-      div.querySelector('progress').setAttribute('value', totalAmount)
-      raised.innerHTML = `£${thousands(totalAmount)}`
-      donors.innerHTML = thousands(totalDonors)
-    })
-  })
+          div.querySelector('progress').setAttribute('value', totalAmount)
+          raised.innerHTML = `£${thousands(totalAmount)}`
+          donors.innerHTML = thousands(totalDonors)
+        })
+
+        setTimeout(grabDonorboxAmounts, 10000)
+      })
+  } catch (_) {
+    // if the fetch failed, for any reason don't worry about it, just try again later
+    setTimeout(grabDonorboxAmounts, 10000)
+  }
+}
+
+grabDonorboxAmounts()
