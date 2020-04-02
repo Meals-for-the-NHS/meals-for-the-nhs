999f12a38abd875ee41c01652a448e3981d48501
Will Gallia <will@stillatplay.com>
2020-04-02 10:20:12 +0100

    use netlify function to grab donorbox amounts

--- functions/pull-donorbox.js
+++ functions/pull-donorbox.js
@@ -1,51 +1,30 @@
 /* eslint-disable */
-const fetch = require("node-fetch");
+const fetch = require("node-fetch")
+
 exports.handler = async function(event, context) {
   try {
-    const response = await fetch("https://icanhazdadjoke.com", {
-      headers: { Accept: "application/json" }
-    });
-    if (!response.ok) {
-      // NOT res.status >= 200 && res.status < 300
-      return { statusCode: response.status, body: response.statusText };
-    }
-    const data = await response.json();
-
+    const email = process.env.DONORBOX_EMAIL
+    const key = process.env.DONORBOX_API_KEY
+    let headers = new fetch.Headers();
+    const btoa = s => Buffer.from(s.toString(), 'binary').toString('base64')
+    headers.append('Authorization', 'Basic ' + btoa(`${email}:${key}`))
+    const url = `https://donorbox.org/api/v1/campaigns`
+    const response = await fetch(url, { headers })
+    const json = await response.json()
+    const [_, campaign] = json
+    let { total_raised, donations_count } = campaign
     return {
       statusCode: 200,
-      body: JSON.stringify({ msg: data.joke })
-    };
+      body: JSON.stringify({
+        donorboxRaised: Math.round(parseFloat(total_raised)),
+        donorboxCount: donations_count
+      })
+    }
   } catch (err) {
-    console.log(err); // output to netlify function log
+    console.log(err) // output to netlify function log
     return {
       statusCode: 500,
-      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
-    };
+      body: JSON.stringify({ msg: JSON.stringify(err.message) })
+    }
   }
-};
-
-
-// (function() {
-//     const email = '{{ helpers.env.DONORBOX_EMAIL }}'
-//     const key = '{{ helpers.env.DONORBOX_API_KEY }}'
-//     let headers = new Headers();
-//     headers.append('Authorization', 'Basic ' + btoa(`${email}:${key}`))
-//     url = `https://donorbox.org/api/v1/campaigns`
-//     const thousands = (s) => s.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").split('.')[0]
-//     if (document.querySelectorAll('.progress-bar')) {
-//         fetch(url, { headers })
-//         .then(r => r.json())
-//         .then((json) => {
-//             const [_, campaign] = json
-//             let { total_raised, donations_count } = campaign
-//             total_raised += {{ total_sponsorship }}
-//             donations_count += {{ total_sponsors }}
-//             var bars = document.querySelectorAll('.progress-bar')
-//             for (i = 0; i < bars.length; i++) {
-//                 bars[i].querySelector('.sponsorship').setAttribute('value', total_raised)
-//                 bars[i].querySelector('p.raised').innerHTML = `£${thousands(total_raised)}`
-//                 bars[i].querySelector('p.donors').innerHTML = thousands(donations_count)
-//             }
-//         })
-//     }
-//   })()
+}
--- resources/js/main.js
+++ resources/js/main.js
@@ -3,3 +3,21 @@
 // import '@modules/mobile-nav'
 import '@modules/lazyload'
 
+const thousands = s => s.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").split('.')[0]
+
+fetch('/api/pull-donorbox')
+  .then(r => r.json())
+  .then((json) => {
+    const { donorboxRaised, donorboxCount } = json
+
+    document.querySelectorAll('div.progress-bar').forEach((div) => {
+      const raised = div.querySelector('p.raised')
+      const totalAmount = parseFloat(raised.dataset.amount) + donorboxRaised
+      const donors = div.querySelector('p.donors')
+      const totalDonors = parseFloat(donors.dataset.amount) + donorboxCount
+
+      div.querySelector('progress').setAttribute('value', totalAmount)
+      raised.innerHTML = `£${thousands(totalAmount)}`
+      donors.innerHTML = thousands(totalDonors)
+    })
+  })
