5200a05cff10269d11916975b35bdefc237d1395
Will Gallia <will@stillatplay.com>
2020-04-15 00:26:37 +0100

    don't download images if we've already got them

--- bin/create-team.py
+++ bin/create-team.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-
-import json, os
+from pathlib import Path
+import json
 import requests
 
 with open("site/globals/data/team.json") as teams_json:
@@ -10,8 +10,12 @@ for record in data["records"]:
     name = record["fields"]["Name"]
     image = record["fields"]["Picture"][0]["thumbnails"]["full"]["url"]
     image_url = (name.replace(" ", "-") + '.png').lower()
+    filepath = Path('images') / 'team' / image_url.lower()
 
-    r = requests.get(image, allow_redirects=True)
-    with open("images/team/" + image_url.lower(), 'wb') as f:
-        f.write(r.content)
-    print("Saving image/" + image_url)
+    if not filepath.exists():
+        r = requests.get(image, allow_redirects=True)
+        with open(filepath, 'wb') as f:
+            f.write(r.content)
+        print("Saving image/" + image_url)
+    else :
+        print(f'already have {filepath}')
