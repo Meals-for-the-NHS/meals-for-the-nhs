84c28c48d5f7196fd1962d7234c1ef2891cac9d8
Will Gallia <will@stillatplay.com>
2020-04-15 10:30:58 +0100

    start showing commented donations, basic template

---
+++ site/includes/scripts/donations.js
@@ -0,0 +1,17 @@
+
+const donationTemplate = document.querySelector('#donation-template')
+donationTemplate.style.display = 'none'
+
+fetch('https://europe-west2-meals4nhs.cloudfunctions.net/api/commented-donations')
+  .then(r => r.json())
+  .then((data) => {
+    const container = document.querySelector('.donations-container')
+    data.donations.forEach((donation) => {
+      const element = donationTemplate.cloneNode(true)
+      for (const key in donation) {
+        element.querySelector(`.${key}`).innerHTML = donation[key]
+      }
+      element.style.display = 'block'
+      container.appendChild(element)
+    })
+  })
