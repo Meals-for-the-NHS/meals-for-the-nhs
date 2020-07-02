59fef7dd8f0ef13606f61a15a1059494e77b4101
Will Tonkin <git@willtonkin.com>
2020-07-02 19:04:26 +0200

    update dashboard and add providers

--- eleventy.config.js
+++ eleventy.config.js
@@ -63,6 +63,7 @@ module.exports = eleventyConfig => {
     eleventyConfig.addLayoutAlias('dashboard', 'layouts/dashboard.njk')
 
     // Include our static assets
+    eleventyConfig.addPassthroughCopy("assets")
     eleventyConfig.addPassthroughCopy("css")
     eleventyConfig.addPassthroughCopy("js")
     eleventyConfig.addPassthroughCopy("images")
